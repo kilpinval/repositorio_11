50ef806f5d8309e41c346bdf7e05d36fbf6b46bb
kilpinval <kilpinval@gmail.com>
2024-11-04 21:31:22 -0500

    Update Vectorizer.sql
    Secon_commit

--- Vectorizer.sql
+++ Vectorizer.sql
@@ -6,3 +6,12 @@ SELECT ai.create_vectorizer(
   chunking => ai.chunking_recursive_character_text_splitter('content'),
   formatting => ai.formatting
 )
+SELECT ai.create_vectorizer(
+  'blog'::regclass,
+  destination => 'blog_contents_embeddings',
+  embedding => ai.embedding_openai('text-embeddig-3-small',768),
+  chunking => ai.chunking_recursive_character_text_splitter('contents', chunk_size => 700),
+  formatting => ai.formatting_python_template('$title - by $author - $chunk'),
+  indexing => ai.indexing_hnsw(min_rows => 100000, opclass => 'vector_12_ops')
+);
+  
